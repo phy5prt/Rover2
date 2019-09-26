506b435843f133c9d12f4496530e3b3bc7814562
phy5prt <philip.r.tate@gmail.com>
2019-09-26 10:19:05 +0100

    Methods in more logical order

--- Rover2Project/Program.cs
+++ Rover2Project/Program.cs
@@ -13,22 +13,22 @@ namespace Rover2Project
         public static void Main(String[] command)
         {
 
-             //this block just catches unit tests
+             //This block just catches unit tests
                 bool unitTesting = (command.Length > 0);
                 if (unitTesting)
                 {
                 Rover myRoverUnitTest = new Rover(unitTesting);
                 String commandStr = (command.Length > 0) ? command[0] : "";
                     myRoverUnitTest.tryExecuteCommandGetResult(myRoverUnitTest.askForValidInputUntilReceivedThenReturnIt(commandStr));
-                    return; //just returning void so unit test doesnt trigger real test
+                    return; //Just returning void so unit test doesnt trigger myRover code after myRoverUnitTest
                 }
-            //end unit test block
+            //End unit test block
 
             Rover myRover = new Rover();
             myRover.interfaceWithUser();
 
         }
-        public class Rover //if in the future there were multiple rovers, they'd need a name string
+        public class Rover //If in the future there were multiple rovers, they'd need a name string
         {
             public struct ResultType {
                 public bool succeeded;
@@ -44,7 +44,7 @@ namespace Rover2Project
             {
                 public int X, Y;
 
-                //later if the coordinates' maximums need to be changed, built in methods in the get and set could protect against
+                //Later if the coordinates' maximums need to be changed, built in methods in the get and set could protect against
                 //moving the maxX to less than the current coordinate or to alter current location if changing orientation or numbering
                 //of the coordinate system
                 public int maxX { get; set; }
@@ -53,7 +53,7 @@ namespace Rover2Project
                 public int minY { get; set; }
 
                 public string lastOrientation;
-                //constructor uses default location and size coordinates
+                //Constructor uses default location and size coordinates
                 
                 public Coordinates(int X = 1, int Y = 1, int maxX = 10, int minX = 1, int maxY = 10, int minY = 1, string lastOrientation = "S")
                 {
@@ -114,7 +114,7 @@ namespace Rover2Project
                     return coords;
                 }
 
-                //Uncomment this section to see how functionality is added
+                //Uncomment this comment block section to see how functionality is added
 
                 //public static Coordinates X(Coordinates coords, int Scale)
                 //{
@@ -122,6 +122,8 @@ namespace Rover2Project
                 //    coords.Y = coords.Y - 1 * Scale;
                 //    return coords;
                 //}
+
+                //End of example method comment block
             }
             public Dictionary<string, MethodInfo> MakeOrientationCommandDictionary(Dictionary<string, MethodInfo> orientationCommands)
             {
@@ -150,6 +152,12 @@ namespace Rover2Project
 
             
             public Coordinates lastCoordinates;
+
+            /// <summary>
+            /// Rover Constructor
+            /// </summary>
+ 
+
             public Rover(bool unitTesting = false)   
             {
                 lastCoordinates = new Coordinates(); //To create a rover with a custom coordinate system, use rover constructor to provide variable for the coordinates constructor
@@ -157,6 +165,32 @@ namespace Rover2Project
                 applyDirectionToDelegate(lastCoordinates.lastOrientation);
                 this.unitTesting = unitTesting;
             }
+
+
+            /// <summary>
+            /// Rover Methods Section in order of calling
+            /// </summary>
+            public String interfaceWithUser()
+            {
+                Console.WriteLine("Rover Activated. Move rover with a sequence of movement commands and direction commands. E.g, 'EMMSM'");
+                String userResponse = "";
+                while (userResponse != "Q" || userResponse != "q")//just incase using breaks to do it so could be set as true
+                {
+
+                    Console.WriteLine("Your current location and orientation is: " + lastCoordinates.getCoordDataShort());
+                    Console.WriteLine($"Please Enter Command:\r\nValid movement commands: { string.Join("", moveActions.Keys.ToArray())}.\r\nValid direction commands:  {string.Join("", orientationCommands.Keys.ToArray())}.\r\nPress return for current status. Or press Q/q to quit.");
+                    userResponse = Console.ReadLine().ToString(); if (userResponse == "Q" || userResponse == "q") { break; }
+                    ResultType result;
+                    while (!(result = tryExecuteCommandGetResult(askForValidInputUntilReceivedThenReturnIt(userResponse))).succeeded) //executed command checks command is in bounds
+                    {
+                        //to get the fail information we have to run the function twice, so either get a resultVariable and a have a do while or turn executedCommand to just return bool without error feedback
+                        Console.WriteLine($"Please re-enter you command:\r\nLast Command exceeded allowed area at {result.failInformation}.\r\nThe rover has not been activated.\r\nValid movement commands: { string.Join("", moveActions.Keys.ToArray())}.\r\nValid direction commands:  {string.Join("", orientationCommands.Keys.ToArray())}.\r\nPress return for current status. ");
+                        userResponse = Console.ReadLine().ToString(); //if (userResponse == "Q") { break; } //would need double break this is where if user has problems could be trapped could be it in function. To avoid nested while loop could use method instead enabling 'break' to work.
+                    };
+                }
+                return userResponse;
+            }
+
 
             public String askForValidInputUntilReceivedThenReturnIt(String input)
             {
@@ -175,38 +209,14 @@ namespace Rover2Project
                 return input;
             }
 
-            public void applyDirectionToDelegate(String ordinate)
-            { 
-                move = (Move)Delegate.CreateDelegate(typeof(Move), orientationCommands[ordinate]);
-            }
-            public String interfaceWithUser()
-            { 
-                Console.WriteLine("Rover Activated. Move rover with a sequence of movement commands and direction commands. E.g, 'EMMSM'");
-                String userResponse = "";
-                while (userResponse != "Q" || userResponse != "q")//just incase using breaks to do it so could be set as true
-                {
-
-                    Console.WriteLine("Your current location and orientation is: " + lastCoordinates.getCoordDataShort());
-                    Console.WriteLine($"Please Enter Command:\r\nValid movement commands: { string.Join("", moveActions.Keys.ToArray())}.\r\nValid direction commands:  {string.Join("", orientationCommands.Keys.ToArray())}.\r\nPress return for current status. Or press Q/q to quit.");
-                    userResponse = Console.ReadLine().ToString(); if (userResponse == "Q" || userResponse == "q") { break; }
-                    ResultType result;
-                    while (!(result = tryExecuteCommandGetResult(askForValidInputUntilReceivedThenReturnIt(userResponse))).succeeded) //executed command checks command is in bounds
-                    {
-                        //to get the fail information we have to run the function twice, so either get a resultVariable and a have a do while or turn executedCommand to just return bool without error feedback
-                            Console.WriteLine($"Please re-enter you command:\r\nLast Command exceeded allowed area at {result.failInformation}.\r\nThe rover has not been activated.\r\nValid movement commands: { string.Join("", moveActions.Keys.ToArray())}.\r\nValid direction commands:  {string.Join("", orientationCommands.Keys.ToArray())}.\r\nPress return for current status. ");
-                            userResponse = Console.ReadLine().ToString(); //if (userResponse == "Q") { break; } //would need double break this is where if user has problems could be trapped could be it in function. To avoid nested while loop could use method instead enabling 'break' to work.
-                    };
-                }
-                return userResponse;
-            }
-
             //Runs test to see if command will go out of bounds
             //If out of bounds, requests new command
             //If command doesn't take out of bounds then applies it to rover
-            public ResultType tryExecuteCommandGetResult(String command) 
+            public ResultType tryExecuteCommandGetResult(String command)
             {
-                Coordinates testRoute = new Coordinates (lastCoordinates.X, lastCoordinates.Y, lastCoordinates.maxX, lastCoordinates.minX, lastCoordinates.maxY, lastCoordinates.minY, lastCoordinates.lastOrientation);
+                Coordinates testRoute = new Coordinates(lastCoordinates.X, lastCoordinates.Y, lastCoordinates.maxX, lastCoordinates.minX, lastCoordinates.maxY, lastCoordinates.minY, lastCoordinates.lastOrientation);
                 Coordinates[] testThenRover = new Coordinates[2] { testRoute, lastCoordinates };
+
                 //The reason we dont just put the rover coordinates in and reset them if they go out of bounds is that the actual rover would have to retrace steps to do so.
                 //It also accommodates the rover having more functionality in the future. 
                 //It may collect data as it goes, or carry out other actions that doesn't effect movement or orientation like take a soil sample or photograph.
@@ -214,8 +224,8 @@ namespace Rover2Project
 
                 for (int j = 0; j < testThenRover.Length; j++)
                 {
-                        
-                        applyDirectionToDelegate(testThenRover[j].lastOrientation); 
+                    applyDirectionToDelegate(testThenRover[j].lastOrientation);
+
                     //Because the test rover and real rover share a delegate it needs to be reset after the test rover has run
                     //This suggests maybe the coordinate system should store its own delegate. 
                     //This would allow different maps different limits, e.g, it had all-terrain mode with sensors protected, it may have a larger map in this mode
@@ -235,11 +245,12 @@ namespace Rover2Project
                         {
                             Console.WriteLine("Uncaught error");
                             ResultType failErrorResult = new ResultType(false);
-                            return failErrorResult; 
+                            return failErrorResult;
                         }
 
                         //If the command has resulted in the test going out of bounds, this information is returned and the rover itself isn't moved
-                        if (j ==0) {
+                        if (j == 0)
+                        {
                             if (testThenRover[j].Y < testThenRover[j].minY || testThenRover[j].Y > testThenRover[j].maxY || testThenRover[j].X > testThenRover[j].maxX || testThenRover[j].X < testThenRover[j].minX)
                             {
                                 String failResultStr = string.Format(" {0}  X = {1}   Y = {2} ", command.Insert(i, "*").Insert(i + 2, "*"), testThenRover[j].X, testThenRover[j].Y);
@@ -254,6 +265,18 @@ namespace Rover2Project
                 ResultType succeedResult = new ResultType(true);
                 return succeedResult;
             }
+
+            public void applyDirectionToDelegate(String ordinate)
+            {
+                move = (Move)Delegate.CreateDelegate(typeof(Move), orientationCommands[ordinate]);
+            }
+
+           
+
+           
+           
+
+            
         }
     }
 }
